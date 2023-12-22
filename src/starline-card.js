cacae4fd1fd5f61a0ca04a8688e24e96abaf8e85
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2023-12-22 15:01:59 +0300

    Card Picker support

--- src/starline-card.js
+++ src/starline-card.js
@@ -464,3 +464,11 @@ class StarlineCard extends HTMLElement {
 }
 
 customElements.define('starline-card', StarlineCard);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+    type: 'starline-card',
+    name: 'StarLine',
+    description: "Amazing card for Starline integration", // Optional
+    documentationURL: "https://github.com/Anonym-tsk/lovelace-starline-card",
+});
